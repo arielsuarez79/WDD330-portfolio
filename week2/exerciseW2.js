921437d553355e533aa73903a94f81e8306193d8
arielsuarez79 <90665089+arielsuarez79@users.noreply.github.com>
2022-05-07 12:46:58 -0300

    week3

--- week2/exerciseW2.js
+++ week2/exerciseW2.js
@@ -5,7 +5,7 @@ function fOne() {
     const outputElement = document.getElementById('f1Output');
 
     outputElement.innerHTML = 'Reverse Number' + reverse_a_number(input);
-  }
+}
 
 
 //this function changes the order of the numbers
